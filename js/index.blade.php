2855af371e9509c41a369493f4024d97b5fb402d
adzkarfauzie <adzkarfauzie02@gmail.com>
2018-07-25 13:41:26 +0700

    Update Registration.js

---
+++ js/index.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TRITON Telkom University</title>
+
+  <link rel="stylesheet" href="{{URL::asset('css/style.min.css')}}">
+
+</head>
+<body>
+  <nav class="navbar is-black is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img class="logo" src="{{URL::asset('pic/triton logo.png')}}" alt="" srcset="">
+        <h1 class="Triton">TRITON</h1>
+      </a>
+
+
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+
+    </div>
+    <div id="nav-menu" class="navbar-menu">
+      <!-- navbar start, navbar end -->
+      <div class="navbar-start">
+        <a target="_blank" href="#" class="navbar-item ">
+          <i class="fab fa-facebook "></i>&nbsp; Facebook</a>
+        <a target="_blank" href="#" class="navbar-item ">
+          <i class="fab fa-instagram "></i>&nbsp; Instagram</a>
+        <a target="_blank" href="#" class="navbar-item ">
+          <i class="fab fa-medium "></i>&nbsp; Blog</a>
+        <a target="_blank" href="#" class="navbar-item ">
+          <i class="fa fa-at "></i>&nbsp; Contact Us</a>
+      </div>
+      <div class="navbar-end">
+        <a target="_blank" href="#" class="navbar-item has-text-info">Login</a>
+        <a target="_blank" href="/registration" class="navbar-item has-text-warning">Register</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="highlight columns">
+    <div class="container">
+      <h1 class="title has-text-white is-1">DARE TO REVOLUTIONIZE INDONESIA ?</h1>
+      <h2 class="title has-text-white is-2">Exclusively for Telkom University Students</h2>
+      presented by
+      <a href="https://telkomuniversity.ac.id/" class="has-text-danger">Telkom</a> University
+      <br>
+      <a target="_blank" href="/registration" class="button is-warning" style="width: 200px; height: 35px;">Register</a>
+    </div>
+
+    <dir class="learn-more container">
+      <strong class="has-text-white title is-3">Learn More Below</strong>
+      <i class="fas fa-chevron-down has-text-white"></i>
+    </dir>
+  </div>
+
+
+  <div class="transition-panel">
+    <h1 class="title is-1 has-text-white">
+      About Triton
+    </h1>
+  </div>
+
+
+  <div class="bg2">
+    <div class="columns" style="padding: 5%">
+      <div class="column" style="text-align: center;">
+        <img src="{{URL::asset('pic/triton logo.png')}}" alt="" width="75%">
+      </div>
+      <div class="column" style="filter: drop-shadow(0px 5px 10px #000); background-color:  rgba(0, 0, 0, 0.5); border-radius: 0 10%">
+          <div style="text-align: left; padding: 10% 0 0   0">
+            Triton ( Telkom Industrial Hackathon), is a national-wide Internet of Things hackathon competition, as a mean to further improve and inovate our industrial sector with the emergence of IoT-based solution
+          </div>
+          <br>
+          <div id="hidden-desc" >
+              we challenge you, the indonesia brightest thinker to show off your IoT based solution and inovation to compete against other in these category:
+              <div style="padding: 10%">
+                  <ul class="columns" style="justify-content: space-between">
+                      <li class="category-item" style="font-size: 30px">
+                        <i class="fa fa-flask"></i>
+                        <p style="font-size: 15px">
+                          Chemistry
+                        </p>
+                      </l>
+                      <li class="category-item" style="font-size: 30px">
+                        <i class="fa fa-shopping-basket"></i>
+                        <p style="font-size: 15px">
+                          Small Micro and <br>Medium Enterprises
+                        </p>
+                      </li>
+                      <li class="category-item" style="font-size: 30px">
+                        <i class="fa fa-heartbeat"></i>
+                        <p style="font-size: 15px">
+                          Health
+                        </p>
+                      </li>
+                      <li class="category-item" style="font-size: 30px">
+                        <i class="fa fa-home"></i>
+                        <p style="font-size: 15px">
+                          Home <br>Automation
+                        </p>
+                      </li>
+
+                    </ul>
+                    <ul class="columns" style="justify-content: space-between">
+                        <li class="category-item" style="font-size: 30px">
+                            <i class="fa fa-lock"></i>
+                            <p  style="font-size: 15px">
+                              Security
+                            </p>
+                          </li>
+                          <li class="category-item" style="font-size: 30px">
+                            <i class="fa fa-gavel"></i>
+                            <p style="font-size: 15px">
+                              Government
+                            </p>
+                          </li>
+                          <li class="category-item" style="font-size: 30px">
+                            <i class="fa fa-dollar-sign"></i>
+                            <p style="font-size: 15px">
+                              Economy
+                            </p>
+                          </li>
+                          <li class="category-item" style="font-size: 30px">
+                            <i class="fa fa-leaf"></i>
+                            <p style="font-size: 15px">
+                              Agriculture
+                            </p>
+                          </li>
+                    </ul>
+              </div>
+
+            </div>
+      </div>
+    </div>
+
+    <div style="text-align: center">
+      <a href="#" class="button is-warning">Download Rule Book</a>
+
+    </div>
+
+  </div>
+
+
+  <div class="transition-panel timel">
+    <h1 class="title is-1 has-text-white">
+      Timeline
+    </h1>
+
+  </div>
+
+  <div class="timeline">
+      <ol>
+          <li>
+            <div>
+              <h3 class="title is-4 has-text-white">10 Juli 2018 – 17 Juli 2018</h3>
+              <p>Registration and Proposal Submition wave 1</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h3 class="title is-4 has-text-white">18 Juli 2018 – 24 Juli 2018</h3>
+              <p>Registration and Proposal Submition wave 2</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h3 class="title is-4 has-text-white">25 Juli 2018 – 31 Juli 2018</h3>
+              <p>Registration and Proposal Submition wave 3</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h3 class="title is-4 has-text-white">10 Agustus 2018</h3>
+              <p>Finalist Announcement</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h3 class="title is-4 has-text-white">18-19 Agustus 2018</h3>
+              <p>Final Hackathon</p>
+            </div>
+          </li>
+      </ol>
+  </div>
+
+
+  <div class="bg3"  style="text-align: center;">
+      <h1 class="title is-1 has-text-white learn-more" style="padding: 30px">Register Your Team Now</h1>
+      <a href="../registration" class="button is-warning" target="_blank">Register Now</a>
+  </div>
+
+  <footer>
+    <div class="columns">
+      <div class="column">
+        <strong class="has-text-warning">Contact Person</strong>
+        <ul>
+          <li>Line: wafazahida17</li>
+          <li>WA: 081212522848</li>
+        </ul>
+      </div>
+      <div class="column">Media Partner</div>
+      <div class="column">Sponsor</div>
+    </div>
+  </footer>
+
+  <script src="{{URL::asset('js/main.min.js')}}"></script>
+  <script src="{{URL::asset('js/responsive.js')}}"></script>
+
+
+</body>
+
+</html>
--- js/registration.js
+++ js/registration.js
@@ -1,198 +1,177 @@
 $().ready(() => {
 
-	var data, listTeamName = [], listEmail = [], getData;
+  isValidTeamName()
+  isValidEmail()
+  isUpload()
+  isValid('team_member_1')
+  isValid('team_member_2')
+  isValid('team_member_3')
+  const team_member_4 = $('input[name=team_member_4]')
+	if(team_member_4.val().length !== 0) isValid('team_member_4')
 
-	// GET Data
-	$.ajax({
-		type: 'GET',
-		dataType: 'json',
-		crossOrigin: true,
-		url: '/api/users',
-		data: data,
-		async: false,
-		headers: {"Accept": "application/json"},
-		success: function(data) {
-			getData = data;
-		},
-		error: function() {
-			console.log('failed to connect API');
-		}
-	});
+  $('button#registrationButton').on('click', () => {
+    if(!$('input[name=agree]')[0].checked)
+      $('#warnAgree').html('Please Check This')
+    else {
+      if(
+        len('input[name=team_name]') &&
+        len('input[name=email]') &&
+        len('input[name=team_member_1]') &&
+        len('input[name=team_member_2]') &&
+        len('input[name=team_member_3]') &&
+        isUploadTrue()
+      )
+      {
+        $(".checkbox").change(function() {
+            if(this.checked) {
+                $('#registrationBtn').removeAttr('disabled');
+            }
+        })
+        $('#warnAgree').html('');
+        const team_name = $('input[name=team_name]'),
+              email = $('input[name=email]'),
+              team_member_1 = $('input[name=team_member_1]'),
+              team_member_2 = $('input[name=team_member_2]'),
+              team_member_3 = $('input[name=team_member_3]'),
+              studentProof = $('input[name=active_student_proof]')[0].files[0]
+        // validating for team_member_4 beacause it nullable
+        var team_member_4 = $('input[name=team_member_4]');
+        team_member_4 = team_member_4.val();
+        if(team_member_4.length === 0) team_member_4 = null;
+        const data = new FormData()
+        data.append('team_name', team_name.val())
+        data.append('email', email.val())
+        data.append('team_member_1', team_member_1.val())
+        data.append('team_member_2', team_member_2.val())
+        data.append('team_member_3', team_member_3.val())
+        data.append('team_member_4', team_member_4)
+        data.append('active_student_proof', studentProof)
+        // axios post
+        document.getElementById('registrationBtn').removeAttribute('disabled');
 
-	// Total Data
-	var totalData = getData.data.length;
-
-	// Assign the Team Name & Email
-	for(var i = 0;i < totalData;i++) {
-    listTeamName[i] = getData.data[i]["Team Name"].toLowerCase();
-    listEmail[i] = getData.data[i]["Email"].toLowerCase();
-  }
-
-	// General Validation
-	function isValid(param) {
-		$(`input[name=${param}]`).on('input', () => {
-			var input = $(`input[name=${param}]`),
-					is_param = input.val();
-			if(is_param) {
-				input.removeClass("is-danger").addClass("is-success");
-				$institution 	   = $(`input[name=${param}]`);
-			}
-				else input.removeClass("is-success").addClass("is-danger");
-		});
-		var input = $(`input[name=${param}]`),
-				is_param = input.val();
-		return (is_param.length > 0);
-	}
-
-	// ValidName Validation
-	function isValidTeamName() {
-		$('input[name=team_name]').on('input', () => {
-			var input = $('input[name=team_name]'),
-					is_teamName = input.val();
-			if(is_teamName.length > 0 && is_teamName.length <= 50 && !listTeamName.includes(input.val().toLowerCase())) {
-				input.removeClass("is-danger").addClass("is-success");
-				$teamName 	   = $('input[name=team_name]');
-				$('#teamNameWarn').html('');
-			}
-				else {
-					input.removeClass("is-success").addClass("is-danger");
-					$('#teamNameWarn').html('This team name has been already taken');
-				}
-			});
-		var input = $('input[name=team_name]'),
-				is_teamName = input.val();
-		return (is_teamName.length > 0) && (is_teamName.length <= 50);
-	}
-
-	// Valid Email Validation
-	function isValidEmail() {
-		var status = false;
-		$('input[name=email]').on('input', () => {
-			var input = $('input[name=email]'),
-			    re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-					is_Email = re.test(input.val());
-			if(is_Email && !listEmail.includes(input.val().toLowerCase())) {
-				input.removeClass("is-danger").addClass("is-success");
-				$teamName 	   = $('input[name=email]');
-				$('#emailWarn').html('');
-				status = true;
-			}
-				else {
-					input.removeClass("is-success").addClass("is-danger");
-					$('#emailWarn').html('This email has been already taken');
-				}
-			});
-		return status;
-	}
-
-	// Valid upload
-	function isUpload() {
-		var input = $('input[name=active_student_proof]');
-		input.on('change', () => {
-			var studentProof = $('input[name=active_student_proof]')[0].files[0],
-					fileName = studentProof.name,
-					fileSize = studentProof.size,
-			// extension validation
-					fileExtension = /\.(zip|rar)$/i;
-			if(!fileExtension.test(fileName) || fileSize > 307200) {
-				var error = "";
-				if(!fileExtension.test(fileName)) error += " | Your File Extension isn't ZIP / RAR \n";
-				if(fileSize > 307200) error += " | Your File Size is bigger than 3MB";
-				input.removeClass("is-success").addClass("is-danger");
-				$('#warn').html(error);
-			}
-			else {
-				$('#warn').html("");
-				input.removeClass("is-danger").addClass("is-success");
-			}
-			return !fileExtension.test(fileName) || fileSize > 307200;
-		});
-	}
-
-	function len(param) {
-		return $(param).val().length > 0;
-	}
-
-	function isUploadTrue() {
-			var studentProof = $('input[name=active_student_proof]')[0].files[0],
-					fileName = studentProof.name,
-			    fileSize = studentProof.size,
-					fileExtension = /\.(rar|zip)$/i;
-			return (fileExtension.test(fileName) && fileSize < 307200);
-	}
-
-	isValidTeamName();
-	isValidEmail();
-	isValid('team_member_1');
-	isValid('team_member_2');
-	isValid('team_member_3');
-	isUpload();
-
-	var team_member_4 = $('input[name=team_member_4]');
-	if(team_member_4.val().length !== 0) isValid('team_member_4');
-
-
-	$('button#registrationButton').on('click', () => {
-
-		// cek checkbox
-		if($('input[name=agree]')[0].checked) {
-
-			// cek length form input jika true baru diinput
-			if(len('input[name=team_name]') && len('input[name=email]') && len('input[name=team_member_1]') && len('input[name=team_member_2]') && len('input[name=team_member_3]') && isUploadTrue()) {
-
-
-				$('#warnAgree').html('');
-
-				var team_name = $('input[name=team_name]');
-				var email = $('input[name=email]');
-				var team_member_1 = $('input[name=team_member_1]');
-				var team_member_2 = $('input[name=team_member_2]');
-				var team_member_3 = $('input[name=team_member_3]');
-				var studentProof = $('input[name=active_student_proof]')[0].files[0];
-
-				// validating for team_member_4 beacause it nullable
-				var team_member_4 = $('input[name=team_member_4]');
-				team_member_4 = team_member_4.val();
-				if(team_member_4.length === 0) team_member_4 = null;
-
-				var data = new FormData();
-				data.append('team_name', team_name.val());
-				data.append('email', email.val());
-				data.append('team_member_1', team_member_1.val());
-				data.append('team_member_2', team_member_2.val());
-				data.append('team_member_3', team_member_3.val());
-				data.append('team_member_4', team_member_4);
-				data.append('active_student_proof', studentProof);
-
-				// Axios Post
-				axios.post('/api/auth/register', data, {
+        axios.post('https://tritontelkom.id/api/auth/register', data, {
 				headers: {
 									'Content-Type': 'multipart/form-data',
 									'Accept': 'Application/json'
 								}})
-									.then((res) => console.log(res))
-									.then(() => {
-										window.location.replace('http://tritontelkom.id/thankyou')
-									})
-									.catch((err) => console.log(err));
+					.then((res) => {
+            window.location.replace('https://tritontelkom.id/thankyou')
+          })
+					.catch((err) => alert('Error\n Please Contact hi@tritontelkom.id'));
+      }
+    } // checkbox
+  })
 
+})
 
-			} else {
-				// Failed
-				isValidTeamName();
-				isValidEmail();
-				isValid('institution');
-				isValid('team_member_1');
-				isValid('team_member_2');
-				isValid('team_member_3');
-				isUpload();
-			}
+const isAvailable = async (param1, data) => {
+  let valid = (param1 === 'team') ? 'isTeamNameAvailable' : ('email') ? 'isEmailAvailable' : 'wrong parameter',
+      url = 'https://tritontelkom.id/api/'+valid+'/'
+  url += (data.length > 0) ? data:'asasa'
+  return await fetch(url)
+}
 
+const isValid = param => {
+  const input = $(`input[name=${param}]`)
+  input.on('input', () => {
+    (input.val().length > 0)
+      ? input.removeClass("is-danger").addClass("is-success")
+      : input.removeClass("is-success").addClass("is-danger")
+    ;
+  })
+}
 
-		} else {
-			$('#warnAgree').html('Please Check This');
-		} // Agar Checkbox di centang
+const isValidTeamName = () => {
+  const input = $('input[name=team_name]')
+  input.on('input', () => {
+    isAvailable('team',input.val())
+      .then(res => res.json())
+      .then(
+        res => {
+          (
+            input.val().length > 0 &&
+            input.val().length <= 50
+          )
+            ? (res.data.Availibility)
+                ? (
+                  input.removeClass("is-danger").addClass("is-success"),
+                  $('#teamNameWarn').html('')
+                )
+                : (
+                  input.removeClass("is-success").addClass("is-danger"),
+                  $('#teamNameWarn').html('This team name has been already taken')
+                )
+            : (
+              input.removeClass("is-success").addClass("is-danger"),
+              $('#teamNameWarn').html('')
+            )
+        }
+      )
+      .catch(err => alert('Error\n Please Contact hi@tritontelkom.id'))
+  })
+}
 
+const isValidEmail = () => {
+  const input = $('input[name=email]'),
+        re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  input.on('input', () => {
+    isAvailable('email', input.val())
+      .then(res => res.json())
+      .then(
+        res => {
+          (
+            re.test(input.val()) &&
+            input.val().length > 0
+          )
+           ? (res.data.Availibility)
+                ? (
+                  input.removeClass("is-danger").addClass("is-success"),
+                  $('#emailWarn').html('')
+                )
+                : (
+                  input.removeClass("is-success").addClass("is-danger"),
+                  $('#emailWarn').html('This email has been already taken')
+                )
+           : (
+             input.removeClass("is-success").addClass("is-danger"),
+             $('#emailWarn').html('')
+           )
+        }
+      )
+      .catch(err => alert('Error\n Please Contact hi@tritontelkom.id'))
+  })
+}
 
-	}); // event on click
-});
+const isUpload = () => {
+  const input = $('input[name=active_student_proof]')
+  input.on('change', () => {
+    const studentProof = $('input[name=active_student_proof]')[0].files[0],
+          fileName = studentProof.name,
+          fileSize = studentProof.size,
+          // extension validation
+          fileExtension = /\.(zip|rar)$/i
+    if(!fileExtension.test(fileName) || fileSize > 3072000) {
+      var error = "";
+      if(!fileExtension.test(fileName)) error += " | Your File Extension isn't ZIP / RAR \n";
+      if(fileSize > 307200) error += " | Your File Size is bigger than 3MB";
+      input.removeClass("is-success").addClass("is-danger");
+      $('#warn').html(error);
+    }
+    else {
+      $('#warn').html("");
+      input.removeClass("is-danger").addClass("is-success");
+    }
+  })
+}
+
+const len = (param) => {
+  return $(param).val().length > 0;
+}
+
+const isUploadTrue = () => {
+  const studentProof = $('input[name=active_student_proof]')[0].files[0],
+        fileName = studentProof.name,
+        fileSize = studentProof.size,
+        fileExtension = /\.(rar|zip)$/i
+  return (fileExtension.test(fileName) && fileSize < 307200)
+}
